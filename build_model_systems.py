87100d91f9a200e8c48f6efabe2f71e26a6e8894
Vincent Voelz <vvoelz@gmail.com>
2024-07-12 15:39:30 -0400

    scripts to build a series of dirs for each model

---
+++ build_model_systems.py
@@ -0,0 +1,47 @@
+import os, sys
+
+### NOTE: This code should be run AFTER parse_models_from_7sa5.py
+
+DEBUG = False
+
+nmodels = 2   # 20
+for i in range(1,nmodels+1):
+
+    # make a new dir called 'model{i}'
+    outdir = f'model{i}'
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+
+    # copy the model PDB to it
+    pdbfile = f'7sa5_model{i}.pdb'
+    os.system(f'cp {pdbfile} {outdir}')
+   
+    # Read in the the runme_VAV_per_model.sh
+    #    ... which has a replacable string "{MODEL_PDB}"
+    fin = open('runme_VAV_per_model.sh', 'r')
+    text = fin.read()
+    fin.close()
+
+    # Replace the runme_VAV_per_model.sh string "{MODEL_PDB}" with the model pdb
+    keyword = "{MODEL_PDB}"
+    text = text.replace(keyword, pdbfile)
+    if DEBUG:
+        print(text)
+        print('\n#################\n')
+
+    # write a new file 'runme.sh' to the new directory
+    outfile = os.path.join(outdir, 'runme.sh') 
+    fout = open(outfile, 'w')
+    fout.write(text)
+    fout.close()
+
+    print(f'Wrote: {outfile}')
+
+
+
+
+
+ 
+
+
+
---
+++ runme_VAV_per_model.sh
@@ -0,0 +1,62 @@
+
+# load in the pathnames to run gmx 2021.2
+module load gromacs/2021.2
+
+# Generates new inputs
+# For NOW we will a topology usig AMBER99 and TIP3P
+echo -e "4\n1\nq" | gmx pdb2gmx -f {MODEL_PDB} -o pin1.gro -p pin1.top -i pin1.itp -n pin1.ndx -ignh
+# on owlsnest: 4 (for AMBFER ff99b) and 1 (for TIP3P)
+
+# THEN, we need to edit the pin1.top file with the #include 
+### ; Include forcefield parameters
+### #include "amber99sbnmr1-ildn.ff/forcefield.itp"
+# replace the string 'amber96.ff' with the string 'amber99sbnmr1-ildn.ff'
+sed -i 's/amber96.ff/amber99sbnmr1-ildn.ff/g' pin1.top
+
+
+# Solvates the system
+gmx editconf -f pin1.gro -o pin1_box.gro -c -d 3.0 -bt cubic
+gmx solvate -cp pin1_box.gro -cs spc216.gro -o pin1_sol.gro -p pin1.top
+## NOTE: the problem before was that we didn't use "spc216.gro" as the solvent box
+
+# Adds ions (makes a tpr using grommp)
+gmx grompp -f ion.mdp -c pin1_sol.gro -p pin1.top -o ion.tpr
+
+# Born et al 2021 says "150 mM sodium chloride buffer at pH 6.5"
+echo -e "13\nq" | gmx genion -s ion.tpr -o pin1_ions.gro -p pin1.top -pname NA -nname CL -neutral -conc 0.150
+# group 13 is "SOL"
+
+
+# Energy minimization (make a tpr using grommp)
+gmx grompp -f em.mdp -c pin1_ions.gro -p pin1.top -o em_large_box.tpr
+
+################################################
+
+# Submit a job to owlsnest 
+qsub em.qsub
+
+# NVT equilibration (make tpr)
+gmx grompp -f nvt.mdp -c em.gro -p pin1.top -o nvt.tpr
+
+# Submit a job to owlsnest 
+qsub nvt.qsub
+
+# Check if it is truly equilibrated 
+echo -e "16\n0\nq" | gmx energy -f nvt.edr -o temperature.xvg
+# 16, then 0; if there is a large drift, redo.
+
+# NPT equilibration (make tpr)
+gmx grompp -f npt.mdp -c nvt.gro -r nvt.gro -t nvt.cpt -p pin1.top -o npt.tpr
+
+# Submit job to owlsnest 
+qsub npt.qsub
+
+# Check if it's equilibrated; remember to select 18 and 0
+echo -e "18\n0\nq" | gmx genion -s ion.tpr -o pin1_ions.gro -p pin1.top -pname NA -nname CL -neutral
+# Check if there is a large drift; if there is, redo. 
+
+# Production run (make tpr)
+gmx grompp -f prod.mdp -c npt.gro -t npt.cpt -p pin1.top -o prod_large_box.tpr
+# Check log file 
+
+~  
